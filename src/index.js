5dcbf44cbcf2e45dee58438522c683501354c655
Will Duncan <willduncan@Wills-MacBook-Pro-63.local>
2024-02-29 20:37:11 -0500

    update

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ const {add} = require('./arithmetica');
 app.use(cors());
 const port = 3000;
 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
 
 app.get('/add/:n/:m', (req, res) => {
     const n = parseInt(req.params.n);
